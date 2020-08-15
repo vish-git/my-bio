a76e798084f05705e0f6c8db615d681d2d598b34
Vishwanath <vishwanathph@gmail.com>
2020-08-15 19:10:32 +0530

    travel diary

--- src/app/tech-dairy/tech-dairy.component.ts
+++ src/app/tech-dairy/tech-dairy.component.ts
@@ -1,15 +1,21 @@
+import { reactLinks } from './../../data/links';
 import { Component, OnInit } from '@angular/core';
+import * as dataLinks from '../links';
 
 @Component({
   selector: 'app-tech-dairy',
   templateUrl: './tech-dairy.component.html',
-  styleUrls: ['./tech-dairy.component.css']
+  styleUrls: ['./tech-dairy.component.css'],
 })
 export class TechDairyComponent implements OnInit {
-
-  constructor() { }
+  reactConceptsLinks: any;
+  reactMainLinks: any;
+  unOrganizedLinks: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.reactConceptsLinks = dataLinks.reactConceptsLinks;
+    this.reactMainLinks = dataLinks.reactMainLinks;
+    this.unOrganizedLinks = dataLinks.unOrganizedLinks;
   }
-
 }
---
+++ src/data/links.ts
@@ -0,0 +1,18 @@
+export const reactLinks = [
+  {
+    title: 'Creating a movie list application - React Projects',
+    link:
+      'https://subscription.packtpub.com/book/web_development/9781789954937/1/ch01lvl1sec04/creating-a-movie-list-application',
+  },
+  {
+    title: 'Creating a movie list application - React Projects',
+    link:
+      'https://subscription.packtpub.com/book/web_development/9781789954937/1/ch01lvl1sec04/creating-a-movie-list-application',
+  },
+];
+
+export const javascriptLinks = {};
+
+export const angularLinks = {};
+export const mongodbLinks = {};
+export const nodejsLinks = {};
