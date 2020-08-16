f5cef9a5765576af643d98d2eaa2be1a3d0922f4
Vishwanath <vishwanathph@gmail.com>
2020-08-16 19:31:14 +0530

    cleaned up the images

--- src/app/links.ts
+++ src/app/links.ts
@@ -633,63 +633,35 @@ const imageDecriptionTemplate = {
 
 export const karnatakaTravelImageLinks = [
   {
-    url: 'assets/my-travel/srigeri.jpg',
-    name: 'Sringeri',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/srigeri.jpg',
-    name: 'Sringeri',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/srigeri.jpg',
-    name: 'Sringeri',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
-  },
-  {
-    url: 'assets/my-travel/agumbe-sunset.jpg',
-    name: 'Agumbe',
+    url: 'assets/my-travel/murudeshwar.jpg',
+    name: 'murudeshwar',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
   },
 ];
-export const keralaTravelImageLinks = [];
+export const keralaTravelImageLinks = [
+  {
+    url: 'assets/my-travel/wayanad.jpg',
+    name: 'Wayanad',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+  },
+];
 
-export const andhraTravelImageLinks = [];
+export const andhraTravelImageLinks = [
+  {
+    url: 'assets/my-travel/Ahobilam.jpg',
+    name: 'Ahobilam',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+  },
+];
 
-export const tamilnaduTravelImageLinks = [];
-export const telanganaTravelImageLinks = [];
+export const tamilnaduTravelImageLinks = [
+  {
+    url: 'assets/my-travel/Hogenakkal.jpg',
+    name: 'Hogenakkal',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+  },
+];
