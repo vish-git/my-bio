b799574c224290f2482bae525dce1f9cc4b6492c
Vishwanath <vishwanathph@gmail.com>
2020-08-20 09:58:23 +0530

    deploy ready site

--- src/app/links.ts
+++ src/app/links.ts
@@ -634,17 +634,15 @@ const imageDecriptionTemplate = {
 export const karnatakaTravelImageLinks = [
   {
     url: 'assets/my-travel/murudeshwar.jpg',
-    name: 'murudeshwar',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+    name: 'Murudeshwar',
+    description: '',
   },
 ];
 export const keralaTravelImageLinks = [
   {
     url: 'assets/my-travel/wayanad.jpg',
     name: 'Wayanad',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+    description: '',
   },
 ];
 
@@ -652,8 +650,7 @@ export const andhraTravelImageLinks = [
   {
     url: 'assets/my-travel/Ahobilam.jpg',
     name: 'Ahobilam',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+    description: '',
   },
 ];
 
@@ -661,7 +658,6 @@ export const tamilnaduTravelImageLinks = [
   {
     url: 'assets/my-travel/Hogenakkal.jpg',
     name: 'Hogenakkal',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dictum leo. Aliquam ut lectus pretium, porttitor nibh nec, pulvinar lorem. Pellentesque semper facilisis magna, vel rutrum dolor iaculis sed. Curabitur et accumsan felis. Ut pellentesque felis ac enim finibus sollicitudin. Pellentesque gravida ultricies sagittis. Vestibulum porta urna vitae dui imperdiet lobortis.',
+    description: '',
   },
 ];
